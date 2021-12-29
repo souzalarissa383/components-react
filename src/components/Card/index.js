0f6d026b7ab4a658db6d16ef9ab61191caf95d88
larissa <souzalarissa383@gmail.com>
2021-12-29 18:20:04 -0300

    primeiro

---
+++ src/components/Card/index.js
@@ -0,0 +1,33 @@
+import {useState} from 'react';
+import Button from '../Button';
+
+const Card = () => {
+    const[valor, setValor] = useState(0)
+
+    function Adicionar() {
+        setValor(valor + 1)
+    }
+
+    function Remover() {
+        setValor(valor - 1)
+    }
+
+    return (
+    <div class="card">
+        <div class="card-header">
+            Primeiro Card
+        </div>
+        <div class="card-body">
+            <Button className="btn btn-success" onClick={Adicionar}>
+                Adicionar
+            </Button>
+            <Button className="btn btn-danger" onClick={Remover}>
+                Remover
+            </Button>
+            <p>{valor}</p>
+        </div>
+    </div>
+    )
+}
+
+export default Card;
---
+++ src/components/Item/index.js
@@ -0,0 +1,9 @@
+const Item = ( {children} ) => {
+        return (
+            //propriedade children
+        <a href="/" className="list-group-item list-group-item-action list-group-item-danger">
+            {children}
+        </a>
+    )
+}
+export default Item;
