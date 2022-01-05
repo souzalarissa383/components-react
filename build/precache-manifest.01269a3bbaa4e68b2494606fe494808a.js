352953cd702b56e1b4df444f43955f6d7bcc3b26
larissa <souzalarissa383@gmail.com>
2022-01-05 15:50:17 -0300

    segundo

---
+++ build/precache-manifest.01269a3bbaa4e68b2494606fe494808a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a1f1adf6be1d99f7062",
+    "url": "/souzalarissa383/components-react/static/js/main.1a1f1adf.chunk.js"
+  },
+  {
+    "revision": "7434611c6f407bbd86a9",
+    "url": "/souzalarissa383/components-react/static/js/1.7434611c.chunk.js"
+  },
+  {
+    "revision": "6f476889258f8e6ba029",
+    "url": "/souzalarissa383/components-react/static/js/runtime~main.6f476889.js"
+  },
+  {
+    "revision": "0ef78e2fb5261e3b7c699faf2bbd8cc8",
+    "url": "/souzalarissa383/components-react/index.html"
+  }
+];
